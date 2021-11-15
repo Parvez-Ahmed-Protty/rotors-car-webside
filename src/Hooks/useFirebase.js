58feedb935b37d588e032036b26cfdf120bb3b13
Parvez-Ahmed-Protty <parvezahmedp22wd@gmail.com>
2021-11-15 12:38:22 +0600

    first

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,67 +1,87 @@
-import initAuth from "../Firebase/init";
-import { GoogleAuthProvider,signInWithPopup,onAuthStateChanged,signOut,createUserWithEmailAndPassword,updateProfile,signInWithEmailAndPassword } from "firebase/auth";
-import { getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { useEffect, useState } from "react";
+import initAuth from "../Firebase/init";
 
-initAuth()
+initAuth();
 
 const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [adminLoading, setAdminLoading] = useState(true);
+  const [admin, setAdmin] = useState(false);
+  // providers
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
-    const [user,setUser] = useState({})
-    const [error,setError] = useState('')
-    const [isLoading, setIsLoading] = useState(true)
-    const [adminLoading, setAdminLoading] = useState(true)
-    const [admin, setAdmin] = useState(false)
-    // providers 
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+  const handleGoogle = () => {
+    setIsLoading(true);
+    return signInWithPopup(auth, provider).finally(() => setIsLoading(false));
+  };
 
-    const handleGoogle = () => {
-        setIsLoading(true)
-        return signInWithPopup(auth, provider)
-        .finally(() => setIsLoading(false))
-    }
+  const logOut = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-      const logOut = () => {
-        setIsLoading(true)
-        signOut(auth).then(() => {
-            setUser({})
-        }).catch((error) => {
-            setError(error.message)
-        })
-        .finally(() => setIsLoading(false))
+  const handleUserRegister = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const handleUserLogin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setAdminLoading(true);
+        fetch(
+          `https://nameless-beyond-24342.herokuapp.com/users/${user?.email}`
+        )
+          .then((res) => res.json())
+          .then((data) => setAdmin(data.admin))
+          .finally(() => setAdminLoading(false));
+      } else {
+        setUser({});
       }
+      setIsLoading(false);
+    });
+    return () => unsubscribe;
+  }, [auth]);
 
-      const handleUserRegister = (email, password) => {
-        return createUserWithEmailAndPassword(auth, email, password)
-    }
-
-    const handleUserLogin = (email, password) => {
-        return signInWithEmailAndPassword(auth, email, password)
-           
-    }
-
-    useEffect(()=>{
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user)
-                setAdminLoading(true)
-                fetch(`https://nameless-beyond-24342.herokuapp.com/users/${user?.email}`)
-                .then(res => res.json())
-                .then(data => setAdmin(data.admin))
-                .finally(()=> setAdminLoading(false))
-            } else {
-                setUser({})
-            }
-            setIsLoading(false)
-          });
-          return () => unsubscribe;
-    },[])
-
-
-    return {
-        handleGoogle,user,error,setUser,setError,logOut,isLoading,handleUserRegister,handleUserLogin,updateProfile,auth,admin,adminLoading
-    }
+  return {
+    handleGoogle,
+    user,
+    error,
+    setUser,
+    setError,
+    logOut,
+    isLoading,
+    handleUserRegister,
+    handleUserLogin,
+    updateProfile,
+    auth,
+    admin,
+    adminLoading,
+  };
 };
 
 export default useFirebase;
--- src/Pages/DashBord/ManegeOrder/ManegeOrder.js
+++ src/Pages/DashBord/ManegeOrder/ManegeOrder.js
@@ -1,46 +1,50 @@
-import React, {useState,useEffect} from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 const ManegeOrder = () => {
-    const [allOrders,setAllOrders] = useState([])
+  const [allOrders, setAllOrders] = useState([]);
 
-    useEffect(()=> {
-        fetch('https://nameless-beyond-24342.herokuapp.com/order')
-        .then(res => res.json())
-        .then(data => setAllOrders(data))
-    },[])
+  useEffect(() => {
+    fetch("https://nameless-beyond-24342.herokuapp.com/order")
+      .then((res) => res.json())
+      .then((data) => setAllOrders(data));
+  }, []);
 
-    const deleteBtn = id => {
-        const confirm = window.confirm('Wanna delete this order of users? Sure?')
-        if (confirm) {
-            fetch(`https://nameless-beyond-24342.herokuapp.com/order/${id}`,{
-            method:'DELETE'
-        }).then(res => res.json())
-        .then(data => {
-            if (data.deletedCount > 0) {
-                alert('Successfully removed item')
-                const newOrders = allOrders.filter(order => order._id !== id)
-                setAllOrders(newOrders)
-            }
-        })
-        }
+  const deleteBtn = (id) => {
+    const confirm = window.confirm("Wanna delete this order of users? Sure?");
+    if (confirm) {
+      fetch(`https://nameless-beyond-24342.herokuapp.com/order/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            alert("Successfully removed item");
+            const newOrders = allOrders.filter((order) => order._id !== id);
+            setAllOrders(newOrders);
+          }
+        });
     }
+  };
 
-    return (
-        <Container>
-            {
-                allOrders.map(order => <div className="p-2" key={order?._id}>
-                    <div className="d-flex align-items-center">
-                        <div className="p-2">
-                                <h5 className="m-0 p-0">{order?.productName.slice(0,10)}</h5>
-                                <p className="m-0 p-0">{order?.name}</p>
-                                <p className="m-0 p-0">{order?.email}</p>
-                                <button className="ms-1" onClick={()=>deleteBtn(order?._id)}>Delete</button><br/>
-                        </div>
-                    </div>
-                </div>)
-            }
-        </Container>
-    );
+  return (
+    <Container>
+      {allOrders.map((order) => (
+        <div className="p-2" key={order?._id}>
+          <div className="d-flex align-items-center">
+            <div className="p-2">
+              <h5 className="m-0 p-0">{order?.productName.slice(0, 10)}</h5>
+              <p className="m-0 p-0">{order?.name}</p>
+              <p className="m-0 p-0">{order?.email}</p>
+              <button className="ms-1" onClick={() => deleteBtn(order?._id)}>
+                Delete
+              </button>
+              <br />
+            </div>
+          </div>
+        </div>
+      ))}
+    </Container>
+  );
 };
 
 export default ManegeOrder;
